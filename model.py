a1d89303e53c15cdda3292b49086f405053202ee
Chetan Pandey <chetanpandey1266@gmail.com>
2020-07-01 10:14:29 +0530

    Output will be a list

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from fastai import *
 from fastai.vision import *
-
+!pip install efficientnet-pytorch
 
 im_path = # location of image(string)
 
@@ -9,4 +9,4 @@ model_path = # loaction of model(string)
 learn = load_learner(model_path)
 
 
-learn.predict(open_image(im_path))
+learn.predict(open_image(im_path))[2].numpy().tolist()
